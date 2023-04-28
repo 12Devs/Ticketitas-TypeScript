320c828838e0e7d335f73dc0bdbd201646a8255a
João Gabriel <gabriel.lima.almeida@gmail.com>
2023-04-27 22:51:21 -0300

    Removendo pastas

---
+++ src/components/NavBarGeral/index.tsx
@@ -0,0 +1,62 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const NavBarGeral = () => {
+  const rotas = [{
+    label: 'Home',
+    to: '/'
+  }, {
+    label: 'Quem somos',
+    to: '/about'
+  }, {
+    label: 'Ajuda',
+    to: '/help'
+  }, {
+    label: 'Seja um promoter',
+    to: '/cadastro-promoter'
+  }];
+
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" className='NavBar'>
+        <Container>
+          <Navbar.Brand>
+            <img
+              src="/img/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block"
+              alt=''
+            />{''}
+            <Link to={'/'} style={{ textDecoration: 'none', color: 'white' }}>
+              {"Ticketitas"}
+            </Link>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto" />
+            <Nav>
+              {/*rotas.map((rota, index) => (
+                <Nav.Item key={index}>
+
+                  <Nav.Link>
+                    <Link to={rota.to} style={{ textDecoration: 'none', color: 'white' }}>
+                      {rota.label}
+                    </Link>
+                  </Nav.Link>
+                </Nav.Item>
+              ))*/}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBarGeral;
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBarGeral from './components/NavBarGeral';
+import React from 'react';
+
+
+export default function AppRouter() {
+    return (
+        <main>
+            <Router>
+                <NavBarGeral/>
+                <Routes>
+                </Routes>
+            </Router>
+        </main>
+    );
+}
