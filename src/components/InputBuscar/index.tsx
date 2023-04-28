4dc345815f961af7dd3644e5c6a3ddf4b313f52c
João Gabriel <gabriel.lima.almeida@gmail.com>
2023-04-27 23:34:31 -0300

    Adicionando algumas componentes

---
+++ src/components/InputBuscar/index.tsx
@@ -0,0 +1,27 @@
+import Form from 'react-bootstrap/Form';
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+
+import './InputBuscar.css';
+import '../Texto/Texto.css';
+
+function InputBuscar({ placeholder, controlId, data, setData}:
+{placeholder: string, controlId: string, data: string, setData: Function}) {
+    return (
+        <Form.Group className="mb-3" controlId={controlId}>
+            <Form.Control
+                type="search"
+                placeholder= {placeholder}
+                aria-label="Barra de pesquisa"
+                className='Input-Primario Texto-Branco'
+                value={data}
+                onChange={(e) => setData(e.target.value)}
+            />
+            <Button variant="none" id="button-addon2">
+                🔍︎
+            </Button>
+        </Form.Group>
+    )
+}
+
+export default InputBuscar;
