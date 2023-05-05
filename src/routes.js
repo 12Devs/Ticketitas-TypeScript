31c8991f856d99f12e6e67724a619850a2bd65eb
Coutilucca <lucca.ahc@gmail.com>
2023-05-04 22:21:47 -0300

    feat: adicionando rota de Admin no route

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import QuemSomos from './pages/QuemSomos';
 import CadastrarCliente from './pages/CadastrarCliente';
 import CadastrarPromoter from './pages/CadastrarPromoter';
+import CadastrarAdmin from './pages/CadastrarAdmin';
 
 
 
@@ -18,6 +19,7 @@ export default function AppRouter() {
                     <Route path='about' element={<QuemSomos/>}/>
                     <Route path='cadastrarSe' element={<CadastrarCliente/>}/>
                     <Route path='cadastrarPromoter' element={<CadastrarPromoter/>}/>
+                    <Route path='cadastrarAdmin' element={<CadastrarAdmin/>}/>
                 </Routes>
                 
             </Router>
