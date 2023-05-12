5ed10d95cb6eda08678b527ec5aa32c9846ac228
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-05-12 17:39:26 -0300

    fix: Finished(?) adding proper route code

--- backend/routes/IndexRoutes.ts
+++ backend/routes/IndexRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { clientRoutes } from "./ClientRoutes";
+import { administratorRoutes } from "./AdministratorRoutes";
 
 const router = Router();
 
 router.use('/user', clientRoutes);
+router.use('/user', administratorRoutes);
 
 export {router};
