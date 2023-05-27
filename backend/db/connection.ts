1bb30945af4729a5c40cf7c33cbedfcb7190cf72
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-05-26 22:56:57 -0300

    refactor: connection.ts now uses env variables

--- backend/db/connection.ts
+++ backend/db/connection.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
+import {DB_HOST as dbhost, DB_USER as dbuser, DB_NAME as dbname, DB_PASSWORD as dbpassword, DB_DIALECT as dbdialect} from '../config/env';
 
-const conn = new Sequelize('ticketitasdb', 'root', '',{
-    host: 'localhost',
-    dialect: 'mysql'
+const conn = new Sequelize(dbname, dbuser, dbpassword,{
+    host: dbhost,
+    dialect: dbdialect
 });
 
 try {
