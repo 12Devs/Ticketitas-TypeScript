b50bd773791b9aa06a1e428d20fa210f04623e26
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-05-26 22:29:03 -0300

    feat: Added env file for system configuration

---
+++ backend/config/env.ts
@@ -0,0 +1,27 @@
+//NODE APP ENVIRONMENT VARIABLES
+const NODE_ENV= "development";
+const PORT= 3333;
+
+//DATABASE CONNECTION ENVIRONMENT VARIABLES
+const DB_HOST= 'localhost';
+const DB_USER= 'root';
+const DB_NAME= 'ticketitasdb';
+const DB_PASSWORD= '';
+const DB_DIALECT= 'mysql';
+
+//SUPER ADMIN DATA
+const SUPER_ADMIN_GENERATION_CODE= "GeraIssoPraMim2023ESNaMalevolenciaDoPaiBola";
+const SUPER_ADMIN_INFO= 
+[
+    {
+        "email": "chefao@email.com", 
+        "password": "umaSenhaParaTodosModificar07457",
+        "name": "Poderoso Chefao",
+        "cpf": 99999999999,
+        "phone": 5575998765432
+    }
+];
+
+export {NODE_ENV, PORT};
+export {DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_DIALECT};
+export {SUPER_ADMIN_GENERATION_CODE, SUPER_ADMIN_INFO};
