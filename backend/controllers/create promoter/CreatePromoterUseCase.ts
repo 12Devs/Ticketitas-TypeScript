029bc6bb1da19202ba1a7db6a98648aed3c5677c
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-05-28 20:39:20 -0300

    chore: Disabled email sending usage just in case...
    The new html body does not accept pure strings

--- backend/controllers/create promoter/CreatePromoterUseCase.ts
+++ backend/controllers/create promoter/CreatePromoterUseCase.ts
@@ -65,9 +65,9 @@ class CreatePromoterUseCase {
         await this.promoterRegistrationRequestRepository.create(nome, email, cpf);
         
         //Message subject text
-        //const subject = "BEM-VINDO, PROMOTOR DE VENDAS";
+        const subject = "BEM-VINDO, PROMOTOR DE VENDAS";
         //Message description text
-        //const message = (`  Caro ${nome}:\n\nÉ com prazer que damos as boas-vindas a um novo promotor em nossa plataforma! Aguarde a liberação por parte um de nossos colaboradores para poder registrar seus eventos;\n\n      Atenciosamente, Equipe Ticketitas.`);
+        const message = (`  Caro ${nome}:\n\nÉ com prazer que damos as boas-vindas a um novo promotor em nossa plataforma! Aguarde a liberação por parte um de nossos colaboradores para poder registrar seus eventos;\n\n      Atenciosamente, Equipe Ticketitas.`);
 
         //Sends information for the "sendEmail" util method to forward the message
         //await this.sendEmail.sendEmail(email, subject, message);
