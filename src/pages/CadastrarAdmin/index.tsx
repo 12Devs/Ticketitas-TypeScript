f3d5a25c3098064273f2bb1dd7d3f791feea4de4
João Gabriel <gabriel.lima.almeida@gmail.com>
2023-06-02 03:23:42 -0300

    feat: Limited pages won't appear for worng users

--- src/pages/CadastrarAdmin/index.tsx
+++ src/pages/CadastrarAdmin/index.tsx
@@ -12,6 +12,7 @@ import NavBarGeral from '../../components/NavBarGeral';
 import { useNavigate } from 'react-router-dom';
 
 import '../pages.css';
+import Forbidden403 from '../Forbidden403';
 
 export default function CadastrarAdmin() {
     const [primeiroNome, setprimeiroNome] = useState('');
@@ -34,57 +35,62 @@ export default function CadastrarAdmin() {
             email
         }
 
-        api.post("/user/administrator", dadosAdmin, config).then((response)=>{console.log(response)});
+        api.post("/user/administrator", dadosAdmin, config).then((response) => { console.log(response) });
 
         navigate('/');
     }
 
-    return (
-        <>
-        <NavBarGeral />
-        <Form style={{minHeight: '75vh'}} onSubmit={realizarCadastro}>
-            <Container>
+    if (localStorage.getItem("userType") == "admin") {
+        return (
+            <>
+                <NavBarGeral />
+                <Form style={{ minHeight: '75vh' }} onSubmit={realizarCadastro}>
+                    <Container>
 
-                <Row>
-                    <FormLabel label='Cadastrar admin'/>
-                </Row> 
+                        <Row>
+                            <FormLabel label='Cadastrar admin' />
+                        </Row>
 
-                <Row>
-                    <Col sm={6}>
-                        <InputTexto type="text" defaultValue={''} required={true} label={"Primeiro nome"} placeholder={""} controlId={"inputPirmeiroNome"} data={primeiroNome} setData={setprimeiroNome} />
-                    </Col>
-                    <Col sm={6}>
-                        <InputTexto type="text" defaultValue={''} required={true} label={"Sobrenome"} placeholder={""} controlId={"inputSobrenome"} data={sobrenome} setData={setSobrenome} />
-                    </Col>
-                </Row>
+                        <Row>
+                            <Col sm={6}>
+                                <InputTexto type="text" defaultValue={''} required={true} label={"Primeiro nome"} placeholder={""} controlId={"inputPirmeiroNome"} data={primeiroNome} setData={setprimeiroNome} />
+                            </Col>
+                            <Col sm={6}>
+                                <InputTexto type="text" defaultValue={''} required={true} label={"Sobrenome"} placeholder={""} controlId={"inputSobrenome"} data={sobrenome} setData={setSobrenome} />
+                            </Col>
+                        </Row>
 
-                <Row>
-                    <Col sm={6}>
-                        <InputTexto type="number" defaultValue={''} required={true} label={"Telefone"} placeholder={""} controlId={"telefone"} data={telefone} setData={setTelefone} />
-                    </Col>
-                    <Col sm={6}>
-                        <InputTexto type="number" defaultValue={''} required={true} label={"CPF"} placeholder={""} controlId={"cpf"} data={newAdminCpf} setData={setCpf} />
-                    </Col>
-                </Row>
+                        <Row>
+                            <Col sm={6}>
+                                <InputTexto type="number" defaultValue={''} required={true} label={"Telefone"} placeholder={""} controlId={"telefone"} data={telefone} setData={setTelefone} />
+                            </Col>
+                            <Col sm={6}>
+                                <InputTexto type="number" defaultValue={''} required={true} label={"CPF"} placeholder={""} controlId={"cpf"} data={newAdminCpf} setData={setCpf} />
+                            </Col>
+                        </Row>
 
-                <Row>
-                    <Col>
-                        <InputTexto type="email" defaultValue={''} required={true} label={"Email"} placeholder={"email@gmail.com"} controlId={"email"} data={email} setData={setEmail} />
-                    </Col>
-                </Row>
+                        <Row>
+                            <Col>
+                                <InputTexto type="email" defaultValue={''} required={true} label={"Email"} placeholder={"email@gmail.com"} controlId={"email"} data={email} setData={setEmail} />
+                            </Col>
+                        </Row>
 
 
-                <Row className='d-flex justify-content-center'>
-                        <Button href='/' style={{margin: '5vh 5vw 5vh 5vw'}} className='Botão-Secundario Texto-Azul'>
-                            Cancelar
-                        </Button>
-                        <Button style={{margin: '5vh 5vw 5vh 5vw'}} className='Botão-Primario Texto-Branco' type="submit" >
-                            Confirmar
-                        </Button>
-                </Row>
+                        <Row className='d-flex justify-content-center'>
+                            <Button href='/' style={{ margin: '5vh 5vw 5vh 5vw' }} className='Botão-Secundario Texto-Azul'>
+                                Cancelar
+                            </Button>
+                            <Button style={{ margin: '5vh 5vw 5vh 5vw' }} className='Botão-Primario Texto-Branco' type="submit" >
+                                Confirmar
+                            </Button>
+                        </Row>
 
-            </Container>
-        </Form>
-        </>
-    )
+                    </Container>
+                </Form>
+            </>
+        )
+    } else {
+        return (<Forbidden403/>);
+    }
+
 }
