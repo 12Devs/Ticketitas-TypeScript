a10ed4bf1ff33ffc6e1ff88813f908a3391cf325
Coutilucca <lucca.ahc@gmail.com>
2023-06-02 11:14:20 -0300

    fix: correction bugs in edition pages

--- src/pages/EditarCliente/index.tsx
+++ src/pages/EditarCliente/index.tsx
@@ -14,12 +14,16 @@ import { useNavigate } from 'react-router-dom';
 import '../pages.css';
 import '../../components/Texto/Texto.css';
 import '../../components/Button/Button.css';
+import jwtDecode from 'jwt-decode';
+
+
 
 export default function EditarCliente() {
+    const [userType, setUserType] = useState('');
+    const [cpf, setCpf] = useState('');
     const [primeiroNome, setprimeiroNome] = useState('');
     const [sobrenome, setSobreome] = useState('');
     const [telefone, setTelefone] = useState('');
-    const [cpf, setCpf] = useState('');
     const [cep, setCep] = useState('');
     const [cidade, setCidade] = useState('');
     const [estado, setEstado] = useState('');
@@ -27,50 +31,55 @@ export default function EditarCliente() {
     const [rua, setRua] = useState('');
     const [numero, setNumero] = useState('');
     const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const [confirmacaoSenha, setConfirmacaoSenha] = useState('');
-    const navigate = useNavigate();
+    
+    const [show, setShow] = useState(false);
+    const [showRecuperar, setShowRecuperar] = useState(false);
+    const handleShowRecuperar = () => {
+        setShowRecuperar(true);
+        setShow(false)
+    };
 
-    const editarCadastro = (event: any) => {
-        event.preventDefault();
-        var dadosCliente: any = {
-            nome: `${primeiroNome} ${sobrenome}`, 
-            telefone,
-            cpf,
-            cep,
-            cidade,
-            estado,
-            bairro,
-            rua,
-            numero,
-            email,
-            senha,
-            confirmacaoSenha
-        }
+    let dados: any;
+    // useEffect(()=>{
+    //     const token = localStorage.getItem('token')
+    //     const user = localStorage.getItem('userType');
+    //     if(token != null){
+    //         dados = jwtDecode(token);
+    //         if(dados != null){
+    //             setCpf(dados.sub);
+    //         }
+    //     }
+    //     if(user != null){
+    //         setUserType(user)
+    //     }
+
+    //     api.get(`user/client/${dados.sub}`).then((response) => {
+    //         console.log(response)
+    //         setprimeiroNome(response.data.ClientInfos.client.nome)
+    //         setEmail(response.data.ClientInfos.client.email)
+    //         setTelefone(response.data.ClientInfos.client.telefone)
+    //         setCep(response.data.enderecoClient.enderecoClient.cep)
+    //         setEstado(response.data.ClientInfos.enderecoClient.estado)
+    //         setBairro(response.data.ClientInfos.enderecoClient.bairro)
+    //         setRua(response.data.ClientInfos.enderecoClient.rua)
+    //         setBairro(response.data.ClientInfos.enderecoClient.bairro)
+    //     });
         
-        api.post("/user/client", dadosCliente).then((response)=>{console.log(response)});
-
-        navigate('/');
         
-    }
+    // },[])
 
-    // Acompanha as mudanças na variavel CEP e chama o conteudo quando ocorrem
-    useEffect(() => {
-        if(cep.length == 8 && !isNaN(parseInt(cep))){
-            
-            api.get(`/endereco/${cep}`).then((endereco) => {
-                setCidade(endereco.data.localidade);
-                setEstado(endereco.data.uf);
-                setBairro(endereco.data.bairro);
-                setRua(endereco.data.logradouro);
-            });
-        }
-    }, [cep]);
+    // const editarCadastro = (event: any) => {
+    //     event.preventDefault();
+    //     api.post(`user/client/${dados.sub}`).then((response) => {
+    //     }
+    // }
+
+
 
     return (
         <>
             <NavBarGeral />
-            <Form style={{minHeight: '75vh'}} onSubmit={editarCadastro}>
+            <Form style={{minHeight: '75vh'}} onSubmit={EditarCliente}>
                 <Container>
 
                     <Row >
@@ -79,55 +88,60 @@ export default function EditarCliente() {
 
                     <Row>
                         <Col sm={6}>
-                            <InputTexto type='text' defaultValue={''} required={true} label={"Primeiro nome"} placeholder={""} controlId={"inputPirmeiroNome"} data={primeiroNome} setData={setprimeiroNome} />
+                            <InputTexto type='text' defaultValue={''} required={true} label={"Primeiro nome"} placeholder={primeiroNome} controlId={"inputPirmeiroNome"} data={primeiroNome} setData={setprimeiroNome} />
                         </Col>
                         <Col sm={6}>
-                            <InputTexto type='text' defaultValue={''} required={true} label={"Sobrenome"} placeholder={""} controlId={"inputSobrenome"} data={sobrenome} setData={setSobreome} />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col sm={6}>
-                            <InputTexto type='number' defaultValue={''} required={true} label={"Telefone"} placeholder={""} controlId={"telefone"} data={telefone} setData={setTelefone}  />
-                        </Col>
-                        <Col sm={6}>
-                            <InputTexto type='number' defaultValue={''} required={true} label={"CPF"} placeholder={""} controlId={"cpf"} data={cpf} setData={setCpf} />
+                            <InputTexto type='text' defaultValue={''} required={true} label={"Sobrenome"} placeholder={sobrenome} controlId={"inputSobrenome"} data={sobrenome} setData={setSobreome} />
                         </Col>
                     </Row>
 
                     <Row>
                         <Col sm={6}>
-                            <InputTexto type='number' defaultValue={''} required={true} label={"CEP"} placeholder={""} controlId={"cep"} data={cep} setData={setCep} />
+                            <InputTexto type='number' defaultValue={''} required={true} label={"Telefone"} placeholder={telefone} controlId={"telefone"} data={telefone} setData={setTelefone}  />
+                        </Col>
+                        <Col sm={6}>
+                            <InputTexto type='number' defaultValue={''} required={true} label={"CPF"} placeholder={cpf} controlId={"cpf"} data={cpf} setData={setCpf} />
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col sm={6}>
+                            <InputTexto type='number' defaultValue={''} required={true} label={"CEP"} placeholder={cep} controlId={"cep"} data={cep} setData={setCep} />
                         </Col>
                     </Row>
 
                     <Row>
                         <Col sm={8}>
-                            <InputTexto type='text' defaultValue={''} required={true} label={"Cidade"} placeholder={""} controlId={"cidade"} data={cidade} setData={setCidade} />
+                            <InputTexto type='text' defaultValue={''} required={true} label={"Cidade"} placeholder={cidade} controlId={"cidade"} data={cidade} setData={setCidade} />
                         </Col>
                         <Col sm={4}>
-                            <InputTexto type='text' defaultValue={''} required={true} label={"Estado"} placeholder={""} controlId={"estado"} data={estado} setData={setEstado} />
+                            <InputTexto type='text' defaultValue={''} required={true} label={"Estado"} placeholder={estado} controlId={"estado"} data={estado} setData={setEstado} />
                         </Col>
                     </Row>
 
                     <Row>
                         <Col>
-                            <InputTexto type='text' defaultValue={''} required={true} label={"Bairro"} placeholder={""} controlId={"bairro"} data={bairro} setData={setBairro} />
+                            <InputTexto type='text' defaultValue={''} required={true} label={"Bairro"} placeholder={bairro} controlId={"bairro"} data={bairro} setData={setBairro} />
                         </Col>
                     </Row>
 
                     <Row>
                         <Col sm={8}>
-                            <InputTexto type='text' defaultValue={''} required={true} label={"Rua"} placeholder={""} controlId={"rua"} data={rua} setData={setRua} />
+                            <InputTexto type='text' defaultValue={''} required={true} label={"Rua"} placeholder={rua} controlId={"rua"} data={rua} setData={setRua} />
                         </Col>
                         <Col sm={4}>
-                            <InputTexto type='text' defaultValue={''} required={true} label={"Número"} placeholder={""} controlId={"numero"} data={numero} setData={setNumero} />
+                            <InputTexto type='text' defaultValue={''} required={true} label={"Número"} placeholder={numero} controlId={"numero"} data={numero} setData={setNumero} />
                         </Col>
                     </Row>
 
                     <Row>
-                        <Col>
-                            <InputTexto type='email' defaultValue={''} required={true} label={"Email"} placeholder={"email@gmail.com"} controlId={"email"} data={email} setData={setEmail} />
+                        <Col sm={8}>
+                            <InputTexto type='email' defaultValue={''} required={true} label={"Email"} placeholder={email} controlId={"email"} data={email} setData={setEmail} />
+                        </Col>
+                        <Col sm={4}>
+                        <Button href='/' style={{margin: '5vh 5vw 5vh 5vw'}} className='Botão-Terciário Texto-Azul'>
+                                Alterar senha
+                            </Button>
                         </Col>
                     </Row>
 
