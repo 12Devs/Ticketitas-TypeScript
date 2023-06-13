bb1c9564d2563bee09e0f93be7bf1fdcb13afce0
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-06-13 17:52:01 -0300

    test: Added remaining coverage for
    LoginAdministratorUseCase

--- backend/controllers/login administrator/LoginAdministratorUseCase.spec.ts
+++ backend/controllers/login administrator/LoginAdministratorUseCase.spec.ts
@@ -11,7 +11,7 @@ import { LoginAdministratorUseCase } from "./LoginAdministratorUseCase";
 //Import of the bcrypt module
 import bcrypt from 'bcrypt';
 
-describe('LoginAdministratorController', () => {
+describe('LoginAdministratorController, returns info when the DB is asked for the existence of an user with a certain email address', () => {
   let loginAdministratorUseCase: LoginAdministratorUseCase;
   let administratorRepository:  AdministratorRepository;
   let tokenAdministratorRepository: TokenAdministratorRepository;
@@ -118,6 +118,126 @@ describe('LoginAdministratorController', () => {
 
     // Verificar se o método findByCpf não foi chamado
     expect(findByEmailAndSenhaSpy).not.toHaveBeenCalled();
+
+    // Verificar se o método create não foi chamado
+    expect(createSpy).not.toHaveBeenCalled();
+  });
+
+  // Teste para observar se uma senha que não coincide com o hash armazenado no BD resulta em falha no login
+  it('should call execute method of LoginAdministratorUseCase, throw a custom error and not return any administrator login info, as the password is incorrect', async () => {
+    
+    // Criação de objetos simulado para a requisição
+    const mockEmail = "youknownothing@email.com";
+    const mockPassword = "thisIsIndeedIncorrect498587";
+    
+    // Espionar os métodos utilizados pela instância de serviço simulada para verificar se foram chamados (ou não)
+    const findByEmailAndSenhaSpy = jest.spyOn(administratorRepository, 'findByEmailAndSenha');
+    const createSpy = jest.spyOn(tokenAdministratorRepository, 'create');
+
+    // Chamar o método execute do serviço com os argumentos desejados e verificar que um erro específico ocorreu
+    try {
+      await loginAdministratorUseCase.execute(mockEmail, mockPassword);
+    }
+    catch(error) {
+      const thrownError = error;
+      
+      //Formular o objeto de erro para comparação
+      const expectedError = new ApiError("Email ou senha incorretos", 422);
+
+      //Comparação dos erros
+      expect(thrownError).toEqual(expectedError);
+    }
+
+    // Verificar se o método findByEmailAndSenha foi chamado com o parâmetro correto
+    expect(findByEmailAndSenhaSpy).toHaveBeenCalledWith(mockEmail);
+
+    // Verificar se o método create não foi chamado
+    expect(createSpy).not.toHaveBeenCalled();
+  });
+
+  // Teste para observar se um erro na coincidência de e-mail resulta em falha do login (tecnicamente impossível, mas quem desenhou este UseCase preferiu assim verifica-lo)
+  it('should call execute method of LoginAdministratorUseCase, throw a custom error and not return any administrator login info, as the there was an error regarding the e-mail stored and that which was searched (virtually impossible, but worth veryfying?)', async () => {
+      
+    // Criação de objetos simulado para a requisição
+    const mockEmail = "maybeyouknowsomething@email.com";
+    const mockPassword = "YeTaNoThErPW987";
+    
+    // Espionar os métodos utilizados pela instância de serviço simulada para verificar se foram chamados (ou não)
+    const findByEmailAndSenhaSpy = jest.spyOn(administratorRepository, 'findByEmailAndSenha');
+    const createSpy = jest.spyOn(tokenAdministratorRepository, 'create');
+
+    // Chamar o método execute do serviço com os argumentos desejados e verificar que um erro específico ocorreu
+    try {
+      await loginAdministratorUseCase.execute(mockEmail, mockPassword);
+    }
+    catch(error) {
+      const thrownError = error;
+      
+      //Formular o objeto de erro para comparação
+      const expectedError = new ApiError("Email ou senha incorretos", 422);
+
+      //Comparação dos erros
+      expect(thrownError).toEqual(expectedError);
+    }
+
+    // Verificar se o método findByEmailAndSenha foi chamado com o parâmetro correto
+    expect(findByEmailAndSenhaSpy).toHaveBeenCalledWith(mockEmail);
+
+    // Verificar se o método create não foi chamado
+    expect(createSpy).not.toHaveBeenCalled();
+  });
+});
+
+describe('LoginAdministratorController, returns no info regarding the provided e-mail address', () => {
+  let loginAdministratorUseCase: LoginAdministratorUseCase;
+  let administratorRepository:  AdministratorRepository;
+  let tokenAdministratorRepository: TokenAdministratorRepository;
+
+  beforeEach(() => {
+    // Criação de um objeto simulado para o caso de uso (AdministratorRepository)
+    administratorRepository = {
+      findByEmailAndSenha: jest.fn() // Utilizamos o jest.fn() para criar uma função simulada
+    } as unknown as AdministratorRepository;
+
+    // Criação de um objeto simulado para o caso de uso (TokenAdministratorRepository)
+    tokenAdministratorRepository = {
+      create: jest.fn(), // Utilizamos o jest.fn() para criar uma função simulada
+    } as unknown as TokenAdministratorRepository;
+
+    // Criação do serviço (LoginAdministratorUseCase) injetando o caso de uso simulado
+    loginAdministratorUseCase = new LoginAdministratorUseCase(
+      administratorRepository,
+      tokenAdministratorRepository
+    );
+  });
+
+  // Teste para observar se um retorno vazio resulta em falha no login
+  it('should call execute method of LoginAdministratorUseCase, throw a custom error and not return any administrator login info, as the password is incorrect', async () => {
+    
+    // Criação de objetos simulado para a requisição
+    const mockEmail = "youknownothing@email.com";
+    const mockPassword = "thisIsIndeedIncorrect498587";
+    
+    // Espionar os métodos utilizados pela instância de serviço simulada para verificar se foram chamados (ou não)
+    const findByEmailAndSenhaSpy = jest.spyOn(administratorRepository, 'findByEmailAndSenha');
+    const createSpy = jest.spyOn(tokenAdministratorRepository, 'create');
+
+    // Chamar o método execute do serviço com os argumentos desejados e verificar que um erro específico ocorreu
+    try {
+      await loginAdministratorUseCase.execute(mockEmail, mockPassword);
+    }
+    catch(error) {
+      const thrownError = error;
+      
+      //Formular o objeto de erro para comparação
+      const expectedError = new ApiError("Email ou senha incorretos", 422);
+
+      //Comparação dos erros
+      expect(thrownError).toEqual(expectedError);
+    }
+
+    // Verificar se o método findByEmailAndSenha foi chamado com o parâmetro correto
+    expect(findByEmailAndSenhaSpy).toHaveBeenCalledWith(mockEmail);
 
     // Verificar se o método create não foi chamado
     expect(createSpy).not.toHaveBeenCalled();
